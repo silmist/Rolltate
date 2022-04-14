39a4ce32964fa45dd3c2c46be2b0ae8aace501d4
Michael Lamore <michael.remorin2023@gmail.com>
2022-04-14 14:17:31 -0700

    Changed border

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -46,7 +46,7 @@ class Play extends Phaser.Scene{
 
         //white borders
         this.add.rectangle(0,0, game.config.width, borderUISize, 0xfacade).setOrigin(0,0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize,0xfacade).setOrigin(0,0);
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0)
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);  
         
